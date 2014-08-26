a1f1ba41d15c3885f0eae3eef5cc6cb3a5d09905
Shuma Yoshioka <s64.stdio@gmail.com>
2014-08-27 08:51:27 +0900

    Update

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 sed -i "s/set \$AIPO_PORT_80_TCP_ADDR .*;\$/set \$AIPO_PORT_80_TCP_ADDR $AIPO_PORT_80_TCP_ADDR;/g" /etc/nginx/conf.d/nginx-reverseproxy-dockerfile.conf
 sed -i "s/set \$AIPO_PORT_80_TCP_PORT .*;\$/set \$AIPO_PORT_80_TCP_PORT $AIPO_PORT_80_TCP_PORT;/g" /etc/nginx/conf.d/nginx-reverseproxy-dockerfile.conf
+
+sed -i "s/set \$AIPO2_PORT_80_TCP_ADDR .*;\$/set \$AIPO2_PORT_80_TCP_ADDR $AIPO2_PORT_80_TCP_ADDR;/g" /etc/nginx/conf.d/nginx-reverseproxy-dockerfile.conf
+sed -i "s/set \$AIPO2_PORT_80_TCP_PORT .*;\$/set \$AIPO2_PORT_80_TCP_PORT $AIPO2_PORT_80_TCP_PORT;/g" /etc/nginx/conf.d/nginx-reverseproxy-dockerfile.conf
+
 nginx -g "daemon off;"
